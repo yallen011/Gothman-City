2abea2a394d7fc94cbce2c1b5086d81529c6d1af
Yvonne Allen <yallen011@gmail.com>
2019-01-07 16:10:20 -0500

    fixed errors and created a schema.json

--- src/gothman-city/index.ts
+++ src/gothman-city/index.ts
@@ -1,7 +1,34 @@
-import { Rule, chain, externalSchematic } from '@angular-devkit/schematics';
+import { Rule, Tree, chain, externalSchematic, SchematicContext } from '@angular-devkit/schematics';
 
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
+import { getWorkspace } from '@schematics/angular/utility/config';
+import { parseName } from '@schematics/angular/utility/parse-name';
+import { findModuleFromOptions } from '@schematics/angular/utility/find-module';
+
+function setupOptions(options: any, host: Tree): void {
+  const workspace = getWorkspace(host);
+  if (!options.project) {
+    options.project = Object.keys(workspace.projects)[0];
+  }
+  const project = workspace.projects[options.project];
+
+  if (options.path === undefined) {
+    const projectDirName = project.projectType === 'application' ? 'app' : 'lib';
+    options.path = `/${project.root}/src/${projectDirName}`;
+  }
+
+  const parsedPath = parseName(options.path, options.name);
+  options.name = parsedPath.name;
+  options.path = parsedPath.path;
+
+}
+
 export function gothmanCity(options: any): Rule {
-  return chain([externalSchematic('@schematics/angular', 'component', options)]);
+  return chain([
+    (tree: Tree, _context: SchematicContext) => {
+      setupOptions(options, tree);
+      options.module = findModuleFromOptions(tree, options) || '';
+      console.log(options);
+    return tree;
+    },
+    externalSchematic('@schematics/angular', 'component', options)]);
 }
