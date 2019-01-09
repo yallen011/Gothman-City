7b89926505b4cbe40b8bd7426f2834b24e27dc32
Yvonne Allen <yallen011@gmail.com>
2019-01-09 12:22:09 -0500

    added schema, schema validation interface, and call to angular service schematic

--- src/gothman-city/index.ts
+++ src/gothman-city/index.ts
@@ -1,5 +1,37 @@
-import { Rule, chain, externalSchematic} from '@angular-devkit/schematics';
+import { Rule, chain, externalSchematic, noop, Tree, SchematicContext} from '@angular-devkit/schematics';
+import { Schema as GothmanOptions } from './schema';
 
-export function gothmanCity(options: any): Rule {
-  return chain([externalSchematic('@schematics/angular', 'component', options)]);
+export interface ServiceOptions {
+  name?: string;
+  flat?: boolean;
+  lintFix?: boolean;
+  project?: string;
+  spec?: boolean;
+}
+
+function convertOptions(options:GothmanOptions): ServiceOptions{
+  const serviceOptions: ServiceOptions = {
+    name: options.service,
+    flat: options.flat,
+    lintFix: options.lintFix,
+    project: options.project,
+    spec: options.spec
+  };
+  return serviceOptions;
+}
+
+function addService(options: GothmanOptions): Rule {
+  return options.service ? externalSchematic('@schematics/angular', 'service', convertOptions(options)) : noop();
+}
+
+function addComponent(options:GothmanOptions): Rule {
+  return externalSchematic('@schematics/angular', 'component', options);
+}
+export function gothmanCity(options: GothmanOptions): Rule {
+  return (host: Tree, context: SchematicContext) => {
+    return chain([
+      addComponent(options),
+      addService(options)
+    ])(host,context);
+  }
 }
